0bb0da4e5938ca48fde72d329014b4f9fc661db3
jennatripoli <jennamt1282@gmail.com>
2022-09-27 20:02:57 -0400

    Button mapping from main activity

---
+++ app/src/main/java/com/example/studybuddy/AddStudySetActivity.kt
@@ -0,0 +1,17 @@
+package com.example.studybuddy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class AddStudySetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_study_set)
+    }
+}
--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.studybuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+private const val REQUEST_CODE_ADD_SET = 1
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonAddStudySet : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAddStudySet = findViewById(R.id.add_study_set)
+
+        buttonAddStudySet.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddStudySetActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_ADD_SET)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+    }
+
 }
 
