27f122d3201cac0889785377b89ab4ae064f1863
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-09-30 00:54:24 -0400

    added declarations to widgets and got refrences

--- app/src/main/java/com/example/studybuddy/AddStudySetActivity.kt
+++ app/src/main/java/com/example/studybuddy/AddStudySetActivity.kt
@@ -2,10 +2,27 @@ package com.example.studybuddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
 
 class AddStudySetActivity : AppCompatActivity() {
+
+    private lateinit var studySetTermList : RecyclerView
+    private lateinit var studySetName : EditText
+    private lateinit var addTerm : Button
+    private lateinit var showFlashCard : Button
+    private lateinit var testStart : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_study_set)
+
+        studySetTermList = findViewById(R.id.study_set_term_list)
+        studySetName = findViewById(R.id.study_set_name)
+        addTerm = findViewById(R.id.add_term)
+        showFlashCard = findViewById(R.id.show_flashcard)
+        testStart = findViewById(R.id.test_start)
+
     }
 }
--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val REQUEST_CODE_ADD_SET = 1
 private const val REQUEST_CODE_POMODORO = 2
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var studySetList : RecyclerView
     private lateinit var buttonAddStudySet : Button
     private lateinit var buttonPomodoro : Button
 
@@ -17,8 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        studySetList = findViewById(R.id.study_set_list)
         buttonAddStudySet = findViewById(R.id.add_study_set)
         buttonPomodoro = findViewById(R.id.study_plan)
+
+        studySetList.layoutManager = LinearLayoutManager(this)
 
         buttonAddStudySet.setOnClickListener {
             val intent = Intent(this@MainActivity, AddStudySetActivity::class.java)
