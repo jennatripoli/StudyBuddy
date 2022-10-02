00eac04740b48923f594668070df4bcac9f4b78f
Samuel Karkache <samkarkachecollege@gmail.com>
2022-10-01 23:53:09 -0400

    wrote more quaries

--- app/src/main/java/com/example/studybuddy/database/DAO.kt
+++ app/src/main/java/com/example/studybuddy/database/DAO.kt
@@ -11,6 +11,12 @@ interface DAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFlashcard(flashcard: FlashcardEntity)
 
+    @Query("SELECT * FROM flashcards")
+    suspend fun getAllFlashcards() : List<FlashcardEntity>
+
+    @Query("SELECT * FROM study_sets")
+    suspend fun getAllStudySets() : List<StudySetEntity>
+
     @Transaction
     @Query("SELECT * FROM study_sets WHERE studySetName = :studySetName")
     suspend fun getStudySetWithFlashcards(studySetName : String): List<StudySetWithFlashcards>
