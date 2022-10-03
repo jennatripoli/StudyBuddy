5ef950063ef6517cafc035c1db328d0bfd78bccc
jennatripoli <jennamt1282@gmail.com>
2022-10-03 15:25:07 -0400

    Added activity for editing terms

--- app/src/main/java/com/example/studybuddy/AddStudySetActivity.kt
+++ app/src/main/java/com/example/studybuddy/AddStudySetActivity.kt
@@ -1,28 +1,32 @@
 package com.example.studybuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import androidx.recyclerview.widget.RecyclerView
+
+private const val REQUEST_CODE_EDIT = 3
 
 class AddStudySetActivity : AppCompatActivity() {
 
-    private lateinit var studySetTermList : RecyclerView
-    private lateinit var studySetName : EditText
     private lateinit var addTerm : Button
-    private lateinit var showFlashCard : Button
-    private lateinit var testStart : Button
+    //private lateinit var editTerm : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_study_set)
 
-        studySetTermList = findViewById(R.id.study_set_term_list)
-        studySetName = findViewById(R.id.study_set_name)
         addTerm = findViewById(R.id.add_term)
-        showFlashCard = findViewById(R.id.show_flashcard)
-        testStart = findViewById(R.id.test_start)
+        //editTerm = findViewById(R.id.term_edit)
 
+        addTerm.setOnClickListener {
+            val intent = Intent(this@AddStudySetActivity, EditStudySetTermActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_EDIT)
+        }
+
+        /*editTerm.setOnClickListener {
+            val intent = Intent(this@AddStudySetActivity, EditStudySetTermActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE_EDIT)
+        }*/
     }
 }
