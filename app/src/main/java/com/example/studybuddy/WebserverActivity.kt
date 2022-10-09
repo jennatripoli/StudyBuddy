496205141ff86f6de514a95f29c6732ea4fc58bb
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2022-10-09 02:31:53 -0400

    implemented microphone speech recognition

--- app/src/main/java/com/example/studybuddy/WebserverActivity.kt
+++ app/src/main/java/com/example/studybuddy/WebserverActivity.kt
@@ -1,7 +1,9 @@
 package com.example.studybuddy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -16,9 +18,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
+import java.util.*
+import kotlin.collections.ArrayList
 
 private const val TAG = "WebserverActivity"
 private const val baseURL = "https://api.dictionaryapi.dev/"
+
+const val REQUEST_CODE_MICROPHONE = 30
 
 class WebserverActivity : AppCompatActivity() {
 
@@ -29,6 +35,14 @@ class WebserverActivity : AppCompatActivity() {
     private lateinit var progressBar : ProgressBar
 
     private lateinit var networkAPI : NetworkAPI
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_MICROPHONE && resultCode == RESULT_OK && data != null) {
+            val spokenArray : ArrayList<String> = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+            editTextSearch.setText(spokenArray.joinToString(separator = " ").trim())
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,6 +65,16 @@ class WebserverActivity : AppCompatActivity() {
 
         buttonMicrophone.setOnClickListener() { // microphone button press
 
+            // create intent
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // put intent extras for speech recognition
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH)
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak Term")
+
+            // start intent activity
+            startActivityForResult(intent, REQUEST_CODE_MICROPHONE)
         }
 
         buttonSearch.setOnClickListener() { // on search
