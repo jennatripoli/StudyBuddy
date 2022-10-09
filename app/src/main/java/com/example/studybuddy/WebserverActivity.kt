76e4897b05bff4625c4522c12f9acb7bf1165c09
Samuel Karkache <samkarkachecollege@gmail.com>
2022-10-08 23:51:08 -0400

    added new dictionary api web idea

---
+++ app/src/main/java/com/example/studybuddy/WebserverActivity.kt
@@ -0,0 +1,15 @@
+package com.example.studybuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+private const val TAG = "WebserverActivity"
+
+class WebserverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webserver)
+
+    }
+}
